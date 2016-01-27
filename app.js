3aa54d0a53082b7957e6e3ad31af2f953a01a11f
Leandro Tomaz Andre <tqi_landre@uolinc.com>
2016-01-27 14:15:42 -0200

    Estruturando melhor o projeto.

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-
-app.get('/produtos', function(request, response){
-	console.log("atendendo a requisição");
-	response.render("produtos/lista");
-	console.log("listando...");
-});
+var app = require('./config/express')();
+var productRoutes = require("./app/routes/produtos")(app);
 
 app.listen(3000, function(){
-	console.log("servidor rodando.")
+	console.log("servidor rodando.");
 });
