24cd48266f2ed67c64938e27f4c32d31a918afa0
Leandro Tomaz Andre <tqi_landre@uolinc.com>
2016-02-16 19:08:40 -0200

    Adicionando o body parser

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,11 +1,27 @@
 module.exports = function(app) {
 	app.get('/produtos', function(request, response){
 		var connection = app.infra.connectionFactory();
-
 		var produtosDao = new app.infra.ProdutosDAO(connection);
 
 		produtosDao.lista(function(err, results){
 			response.render('produtos/lista', {lista: results});
+		});
+
+		connection.end();
+	});
+
+	app.get('/produtos/form', function(request, response) {
+		response.render('produtos/form');
+	});
+
+	app.post('/produtos', function(request, response) {
+		var produto = request.body;
+
+		var connection = app.infra.connectionFactory();
+		var produtosDao = new app.infra.ProdutosDAO(connection);
+
+		produtosDao.salva(produto, function(err, results){
+			response.redirect('/produtos');
 		});
 
 		connection.end();
