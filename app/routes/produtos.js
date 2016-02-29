074e5c4c7cfc3d1b0cb1f759e2382ffaef9feed4
Leandro Tomaz Andre <tqi_landre@uolinc.com>
2016-02-29 19:27:25 -0300

    Criando testes com supertest

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -4,24 +4,48 @@ module.exports = function(app) {
 		var produtosDao = new app.infra.ProdutosDAO(connection);
 
 		produtosDao.lista(function(err, results){
-			response.render('produtos/lista', {lista: results});
+			response.format({
+				html: function() {
+					response.render('produtos/lista', {lista: results});
+				},
+				json: function() {
+					response.json(results);
+				}
+			});
 		});
 
 		connection.end();
 	});
 
 	app.get('/produtos/form', function(request, response) {
-		response.render('produtos/form');
+		response.render('produtos/form', {errosValidacao:{}, produto:{}});
 	});
 
-	app.post('/produtos', function(request, response) {
-		var produto = request.body;
+	app.post('/produtos', function(req, res) {
+		var produto = req.body;
+
+		req.assert('titulo', 'Título é obrigatório').notEmpty();
+		req.assert('preco', 'Formato inválido').isFloat();
+
+		var erros = req.validationErrors();
+		if(erros) {
+			res.format({
+				html: function() {
+					res.status(400).render('produtos/form',{errosValidacao:erros, produto:produto});
+				},
+				json: function() {
+					res.status(400).json(erros);
+				}
+			});
+
+			return;
+		}
 
 		var connection = app.infra.connectionFactory();
 		var produtosDao = new app.infra.ProdutosDAO(connection);
 
 		produtosDao.salva(produto, function(err, results){
-			response.redirect('/produtos');
+			res.redirect('/produtos');
 		});
 
 		connection.end();
